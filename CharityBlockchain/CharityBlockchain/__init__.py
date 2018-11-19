a27d4ab4ad2969d613bc79b4960a2afd7d5e6f72
cl <13546417565@163.com>
2018-11-20 02:09:56 +0800

    添加数据库，初始化注册登录界面

--- CharityBlockchain/CharityBlockchain/__init__.py
+++ CharityBlockchain/CharityBlockchain/__init__.py
@@ -1,8 +1,56 @@
 """
 The flask application package.
 """
-
+import os
 from flask import Flask
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_mapping(SECRET_KEY='qetasg',#TODO
+            DATABASE=os.path.join(app.instance_path,'CharityBlockchain.sqlite'))
+    from CharityBlockchain import db
+    db.init_app(app)
+    from CharityBlockchain import user
+    app.register_blueprint(user.user)
+    import CharityBlockchain.views
+    from datetime import datetime
+    from flask import render_template
+    #from CharityBlockchain import app
 
-import CharityBlockchain.views
+    @app.route('/')
+    @app.route('/home')
+    def home():
+        """Renders the home page."""
+        return render_template('index.html',
+            title='Home Page',
+            year=datetime.now().year,)
+
+    @app.route('/contact')
+    def contact():
+        """Renders the contact page."""
+        return render_template('contact.html',
+            title='联系我们',
+            year=datetime.now().year,
+            message='联系页面')
+
+    @app.route('/about')
+    def about():
+        """Renders the about page."""
+        return render_template('about.html',
+            title='关于',
+            year=datetime.now().year,
+            message='区块链公益')
+
+    @app.route('/sign_in')
+    def sign_in():
+        """Sign in the System"""
+        return render_template('sign_in.html',
+            tilte='登录',
+            year=datetime.now().year)
+
+    @app.route('/sign_up')
+    def sign_up():
+        """Sign up the System"""
+        return render_template('sign_up.html',
+            tilte='注册',
+            year=datetime.now().year)
+    return app
--- CharityBlockchain/runserver.py
+++ CharityBlockchain/runserver.py
@@ -3,12 +3,12 @@ This script runs the CharityBlockchain application using a development server.
 """
 
 from os import environ
-from CharityBlockchain import app
-
+from CharityBlockchain import create_app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
+    app = create_app()
     app.run(HOST, PORT)
